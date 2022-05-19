3e2da8e42294ba6529b483a273d0e7fe7152aa89
thoo-ma <youngbenjaminhorne@icloud.com>
2022-05-19 18:32:25 +0200

    trees

--- include/utility/rb_tree.hpp
+++ include/utility/rb_tree.hpp
@@ -7,167 +7,200 @@
 
 namespace ft {
 
-template <typename T>
-class rb_node
-{
-    public:
-
-    typedef T	                key_type;
-    typedef enum { Black, Red } color_type;
-    typedef rb_node<T> *        pointer;
-
-    // private ?
-    key_type    key;
-    color_type  color;
-    rb_node * 	left;
-    rb_node *	right;
-    rb_node *	parent;
-
-    rb_node(key_type const & val)
-    : key(val), color(Black), left(NULL), right(NULL), parent(NULL)
-    { }
-
-//    rb_node(const key_type & key, const color_type & color,
-//            pointer left, pointer right, pointer parent)
-//    : key(key), color(color), left(left), right(right), parent(parent)
-//    { }
-
-//    rb_node<key_type> &
-//    operator=(const rb_node<key_type> & a)
-//    {
-//        key = a.key;
-//        color = a.color;
-//        left = a.left;
-//        right = a.right;
-//        parent = a.parent;
-//        return *this;
-//    }
-
-};
-
-template <typename T>
-bool
-operator==(const rb_node<T> & lhs, const rb_node<T> rhs)
-{ return lhs.key == rhs.key; }
-
-template <typename T>
-std::ostream &
-operator<<(std::ostream & o, rb_node<T> const & node)
-{ o << node.key; return o;}
-
 template <typename T,
-          //typename Compare = std::less<T>,
-          typename Allocator = std::allocator<rb_node<T>>
+          typename Compare = std::less<T>
+//          typename Allocator = std::allocator<struct node>
 >
-class rb_tree {
-
-    /*
-     *	RULES
-     * 	_____
-     *
-     * 	1. Every node is Red or Black (Color property)
-     * 	2. Every null node is considered Black (Null property)
-     * 	3. A Red node does not have a Red child (Red  proterty)
-     * 	4. Every path from a given node to any of its descendant null nodes
-     *    goes through the same number of black nodes. (Black property)
-     *
-     *	OPERATIONS
-     *	__________
-     *
-     *	- search (lookup)
-     *	- insert (6 cases)
-     *	- delete (6 cases)
-     *
-     *	GLOSSARY
-     *	________
-     *
-     *  - path
-     *  - leaf: null node
-     *	- node's black depth: number of black nodes from root to that node
-     *	- tree's black height: number of black nodes in any path from root to leaves
-     *
-     *	cf. https://en.wikipedia.org/wiki/Red%E2%80%93black_tree#Properties
-     *
-     */
-
+class rb_tree
+{
     /****** Member types ******************************************************/
 
     public:
 
-    typedef T	            key_type;
-    typedef Allocator       allocator_type;
-    typedef rb_node<T>	    node_type; // value_type ?
-    typedef rb_node<T> *	pointer;
-    typedef rb_node<T> &	reference;
+    struct node; // forward declaration
 
-    typedef enum { Left, Right } direction;
-    typedef enum { Black, Red } color_type;
+    typedef struct node	                node_type; // value_type ?
+    typedef struct node *	            pointer;
+    typedef struct node &               reference;
+    typedef T	                        key_type;
+    typedef Compare	                    compare;
+    typedef std::allocator<struct node>	allocator_type;
+    typedef enum { Black, Red }	        color_type;
 
-    /****** Internal data *****************************************************/
+    struct node
+    {
+        key_type	key;
+        color_type	color;
+        pointer     left;
+        pointer     right;
+        pointer     parent;
+
+        node(const key_type & key = key_type())
+        : key(key), color(Red), left(NULL), right(NULL), parent(NULL) { }
+
+    };
+
+    /****** Private variables *************************************************/
 
     private:
 
     pointer         _root;
+    compare	        _comp;
     allocator_type	_alloc;
 
-    void _print(pointer node) const
+    /****** Private functions *************************************************/
+
+    // utility for recursive fashion destructor
+    void _destroy(const pointer node)
     {
-        if (node == NULL) return;
-        if (node->left)
-            _print(node->left);
-        if (node->right)
-            _print(node->right);
-        std::cout << node->key << std::endl;
+        if (node == NULL)
+            return;
+        _destroy(node->left);
+        _destroy(node->right);
+        _alloc.destroy(node);
+        _alloc.deallocate(node, 1);
     }
 
-    void	_debug_insert(pointer node) const
+    pointer _get_brother(pointer node)
     {
-        std::cout << "inserted node key: " << node->key << std::endl;
-        std::cout << "inserted node color: " << node->color << std::endl;
-        std::cout << "inserted node parent: " << node->parent << std::endl;
-        if (node->parent)
+        return node == node->parent->left
+        ? node->parent->right
+        : node->parent->left;
+    }
+
+    void left_rotate(pointer node)
+    {
+        if (node->right)
         {
-            node == node->parent->left
-            ? std::cout << "inserted node directon: left" << std::endl
-            : std::cout << "inserted node directon: right" << std::endl;
+            if (node->parent)
+            {
+                node->parent->left == node
+                ? node->parent->left = node->right
+                : node->parent->right = node->right;
+            }
+            node->right->parent = node->parent;
+            node->parent = node->right;
+            node->right = node->right->left;
+            node->parent->left = node;
+            if (node == _root)
+                _root = node->parent;
         }
-        else { std::cout << "inserted node directon: root" << std::endl; }
+    }
+
+    void right_rotate(pointer node)
+    {
+        if (node->left)
+        {
+            if (node->parent)
+            {
+                node->parent->left == node
+                ? node->parent->left = node->left
+                : node->parent->right = node->left;
+            }
+            node->left->parent = node->parent;
+            node->parent = node->left;
+            node->left = node->left->right;
+            node->parent->right = node;
+            if (node == _root)
+                _root = node->parent;
+        }
+    }
+
+    void left_balance(pointer node, pointer parent, pointer uncle)
+    {
+        // case 1: uncle is Red
+        if (uncle && uncle->color == Red)
+        {
+            uncle->color = Black;
+            parent->color = Black;
+            parent->parent->color = Red;
+        }
+        else
+        {
+            // case 3: node is right-child of parent
+            if (node == parent->right)
+                left_rotate(parent);
+            // case 2: node is left-child of parent
+            right_rotate(parent->parent);
+            node->parent->color = Black;
+            node->parent->right->color = Red;
+        }
+    }
+
+    void right_balance(pointer node, pointer parent, pointer uncle)
+    {
+        // case 1: uncle is Red
+        if (uncle && uncle->color == Red)
+        {
+            uncle->color = Black;
+            parent->color = Black;
+            parent->parent->color = Red;
+        }
+        else
+        {
+            // case 3: node is right-child of parent
+            if (node == parent->right)
+                right_rotate(parent);
+            // case 2: node is left-child of parent
+            left_rotate(parent->parent);
+            node->parent->color = Black;
+            node->parent->left->color = Red;
+        }
     }
 
     /****** Member functions **************************************************/
 
     public:
 
-    pointer & root() { return _root; }
+    // utility (might delete later)
+    pointer * root_ptr() { return &_root; }
+    pointer & root_ref() { return  _root; }
+    pointer  root_node() { return  _root; }
 
-    rb_tree() : _root(NULL) { }
+    // utility (might delete later)
+    void print(const pointer node)
+    {
+        if (node == NULL)
+            return;
 
-    rb_tree(const rb_tree & tree) { (void)tree; }
+        node->color == Red
+        ? std::cout << "\33[31m" << node->key << "\33[0m"
+        : std::cout << "\33[30m" << node->key << "\33[0m";
 
-    // inline ? macro ?
-    direction get_dir(reference node) const
-    { return node == node->parent->left ? Left : Right; }
+        if (node->left || node->right)
+        {
+            std::cout << '(';
+            print(node->left);
+            if (node->right)
+            {
+                std::cout << ',';
+                print(node->right);
+            }
+            std::cout << ')';
+        }
+    }
 
+    rb_tree() : _root(NULL), _comp(compare()), _alloc(allocator_type()) { }
+
+    //rb_tree(const rb_tree & tree) { (void)tree; }
+
+    // for-loop fashion
     ~rb_tree()
     {
         pointer	    node;
         pointer     parent;
-        direction	dir;
 
         node = _root;
-        dir = Left;
         while (node)
         {
             parent = node->parent;
             while (node && node->left)
             {
-                dir = Left;
                 parent = node;
                 node = node->left;
             }
             if (node && node->right)
             {
-                dir = Right;
                 parent = node;
                 node = node->right;
             }
@@ -175,41 +208,101 @@ class rb_tree {
             {
                 _alloc.destroy(node);
                 _alloc.deallocate(node, 1);
+                if (parent && node == parent->left) parent->left = NULL;
+                if (parent && node == parent->right) parent->right = NULL;
                 node = parent;
-                if (node) { dir == Left ? node->left = NULL : node->right = NULL; }
             }
         }
     }
 
-    void print() const { return _print(_root); }
+    // recursive fashion
+    //~rb_tree() { _destroy(_root); }
 
-    key_type insert(pointer & node, pointer & parent, const key_type & key)
+    void delete_node(pointer node)
     {
-        // insert
+        // case 1: node has no right child
+        if (node->right == NULL)
+        {
+            if (node->parent)
+            {
+                node->parent->left == node
+                ? node->parent->left = node->left
+                : node->parent->right = node->left;
+            }
+            if (node->left)
+                node->left->parent = node->parent;
+            // not sure
+            _alloc.destroy(node);
+            _alloc.deallocate(node);
+            return;
+        }
+
+        // case 2: node's right child has no left child
+        if (node->right->left == NULL)
+        {
+            if (node->parent)
+            {
+                node->parent->left == node
+                ? node->parent->left = node->right
+                : node->parent->right = node->right;
+            }
+            node->right->parent = node->parent;
+            node->right->left = node->left;
+            if (node->left)
+                node->left->parent = node->right;
+            // not sure
+            _alloc.destroy(node);
+            _alloc.deallocate(node);
+        }
+
+        // case 3: node's right child has a left child
+        else
+        {
+            // find inorder sucecssor
+            pointer succ = node->right->left;
+            while (succ->left) { succ = succ->left; }
+
+            // delete succ
+            succ->parent->left = succ->right;
+            if (succ->right)
+                succ->right->parent = succ->parent;
+
+            // replace node by succ
+            node->key = succ->key;
+
+            // not sure
+            _alloc.destroy(node);
+            _alloc.deallocate(node);
+        }
+    }
+
+    // with a recursive fashion search
+    void insert(pointer & node, pointer parent, key_type & key)
+    {
+        // insert at leaf -- or root
         if (node == NULL)
         {
-            pointer tmp = _alloc.allocate(1);
-            // insert at root
-            if (node == _root)
+            // allocate & construct `node` from `key`
+            node = _alloc.allocate(1);
+            _alloc.construct(node, node_type(key));
+            // insert `node` below `parent`
+            node->parent = parent;
+            if (parent)
             {
-                _alloc.construct(tmp, key);
-                _root = tmp;
+                key < parent->key
+                ? parent->left = node
+                : parent->right = node;
             }
-            // insert at leaf
-            else
+            else return;
+            // rebalance
+            while (parent->parent && parent->color == Red)
             {
-                _alloc.construct(tmp, key);
-                // udate parent.left or parent.right
-                tmp->parent = parent;
-                // fix this cast (only one enum), rb_node as subclass (or maybe coloring into constructor)
-                tmp->color = static_cast<typename rb_node<T>::color_type>(Red);
-                node = tmp;
-                // case 1
-           //     if ((*parent)->color == Black) { return key; } // insertion complete
-           //     if ((*parent)->parent == NULL) { (*parent)->color = Red; return; } // case 4. insertion complete
+                node == parent->left
+                ?  left_balance(node, parent, _get_brother(parent))
+                : right_balance(node, parent, _get_brother(parent));
             }
-            _debug_insert(node);
-            return key;
+            _root->color = Black;
+            return;
         }
         // recursive until leaf
         return key < node->key
@@ -217,14 +310,16 @@ class rb_tree {
         : insert(node->right, node, key);
     }
 
-    // Since rb_tree support duplicate keys, what do we return ? 1st one or all equivalent keys ?
-    pointer find(pointer node, const key_type & key) const
+    // recursive fashion
+    // This Red Black Tree support duplicate keys.
+    // This function return the first equivalent key when there are.
+    pointer find(const pointer node, const key_type & key) const
     {
-        if (node == _root)
-            return NULL;
-        if (node->key == key)
+        if (node == NULL || node->key == key)
             return node;
-
+        return key < node->key
+        ? find(node->left, key)
+        : find(node->right, key);
     }
 
 };
--- src/output/output_rb_tree.cpp
+++ src/output/output_rb_tree.cpp
@@ -1,37 +1,20 @@
 #include "rb_tree.hpp"
 
-void rb_tree_print_test()
-{
-    ft::rb_tree<int> tree;
-
-  //  ft::rb_node<int> ** root = tree.root();
-
-  //  std::cout << "root addr: " << root << std::endl;
-  //  std::cout << "root node addr: " << *root << std::endl;
-
-    tree.insert(tree.root(), tree.root()->parent, 21);
-
-  //  std::cout << "root addr: " << root << std::endl;
-  //  std::cout << "root node addr: " << *root << std::endl;
-
-    tree.insert(tree.root(), tree.root()->parent, 42);
-
-  //  std::cout << "root addr: " << root << std::endl;
-  //  std::cout << "root node addr: " << *root << std::endl;
-  //  std::cout << "root node left addr: " << (*root)->left << std::endl;
-  //  std::cout << "root node right addr: " << (*root)->right << std::endl;
-
-   // tree.print();
-
-    //ft::rb_tree<std::pair<int,double>> t;
-
-   // for (int i = 0; i < 10; i++)
-   //     tree.insert(i);
-
-}
-
 int main()
 {
-    rb_tree_print_test();
+    {
+        ft::rb_tree<int> tree;
+        for (int i = 0; i < 6; i++) {
+            tree.insert(tree.root_ref(), NULL, i);
+            tree.print(tree.root_node()); std::cout << std::endl;
+        }
+    }
+    {
+        ft::rb_tree<int> tree;
+        for (int i = 6; i > 0; i--) {
+            tree.insert(tree.root_ref(), NULL, i);
+            tree.print(tree.root_node()); std::cout << std::endl;
+        }
+    }
     return 0;
 }
